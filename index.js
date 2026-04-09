3cfc15acf568824ee0a641196486e43f86e5beb9
BoGhazi09 <faladasany@gmail.com>
2026-04-09 21:17:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -32,6 +32,32 @@ client.on("interactionCreate", async (interaction) => {
   if (!interaction.isChatInputCommand()) return;
 
   if (interaction.commandName === "claimticket") {
+  const allowedRoleId = "1478564123259310090";
+
+  const member = interaction.member;
+
+  if (!member.roles.cache.has(allowedRoleId)) {
+    return interaction.reply({
+      content: "You can't use this command.",
+      ephemeral: true
+    });
+  }
+
+  const channel = interaction.channel;
+
+  const username = interaction.user.username
+    .toLowerCase()
+    .replace(/[^a-z0-9]/g, "");
+
+  const newName = `${channel.name}-${username}`;
+
+  await channel.setName(newName);
+
+  return interaction.reply({
+    content: `Ticket claimed by ${interaction.user.username}`,
+    ephemeral: true
+  });
+}
     const channel = interaction.channel;
 
     const username = interaction.user.username
